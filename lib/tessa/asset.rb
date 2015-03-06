54f07042dc5268d1fde5a15b7a3728f63ac2c102
Travis Petticrew <travis@petticrew.net>
2015-03-06 15:20:56 -0600

    Removes old code
    The purpose of this codebase is now completely different than originally
    intended. We will start from empty classes.

--- lib/tessa/asset.rb
+++ lib/tessa/asset.rb
@@ -1,28 +1,4 @@
-require 'uri'
-require 'securerandom'
-
 module Tessa
   class Asset
-    attr_reader :uuid, :metadata, :uri
-
-    def initialize(uuid:, metadata: nil, uri: nil)
-      @uuid = uuid
-      @metadata = metadata
-      @uri = URI(uri || "")
-    end
-
-    def download(backend_db: Backend.db)
-      raise if uri.scheme.nil?
-      backend = backend_db.fetch(uri.scheme)
-      backend.download(uri)
-    end
-
-    def upload(file, backend: Tessa.config.default_backend)
-      @uri = backend.upload(file)
-    end
-
-    def self.create
-      new(uuid: SecureRandom.uuid)
-    end
   end
 end
--- lib/tessa/config.rb
+++ lib/tessa/config.rb
@@ -1,13 +1,4 @@
 module Tessa
   class Config
-    attr_accessor :backends, :default_backend
-
-    def initialize(options = {})
-      @backends = {}
-    end
-
-    def default_backend
-      @backends[@default_backend]
-    end
   end
 end
