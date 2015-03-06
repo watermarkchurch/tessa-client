8eaaf2eebb1dff2bc94999a96ead618afbe0cbb3
Travis Petticrew <travis@petticrew.net>
2015-03-06 15:41:10 -0600

    Adds config attributes
    Starting with username, password, and default_strategy.

--- lib/tessa/config.rb
+++ lib/tessa/config.rb
@@ -1,4 +1,12 @@
 module Tessa
   class Config
+    attr_accessor :username, :password
+    attr_accessor :default_strategy
+
+    def initialize
+      @username ||= ENV['TESSA_USERNAME']
+      @password ||= ENV['TESSA_PASSWORD']
+      @default_strategy ||= ENV['TESSA_DEFAULT_STRATEGY']
+    end
   end
 end
--- spec/tessa/config_spec.rb
+++ spec/tessa/config_spec.rb
@@ -2,4 +2,53 @@ require 'spec_helper'
 
 RSpec.describe Tessa::Config do
   subject(:config) { Tessa::Config.new }
+
+  shared_examples_for "defaults to environment variable" do
+    around { |ex| swap_environment_var(variable_name, 'from-env') { ex.run } }
+
+    it { is_expected.to eq("from-env") }
+
+    def swap_environment_var(var, new_val)
+      old_val = ENV[var]
+      ENV[var] = new_val
+      yield
+      ENV[var] = old_val
+    end
+  end
+
+  describe "#username" do
+    it_behaves_like "defaults to environment variable" do
+      let(:variable_name) { 'TESSA_USERNAME' }
+      subject(:username) { config.username }
+    end
+
+    it "behaves like a normal accessor" do
+      config.username = "my-new-value"
+      expect(config.username).to eq("my-new-value")
+    end
+  end
+
+  describe "#password" do
+    it_behaves_like "defaults to environment variable" do
+      let(:variable_name) { 'TESSA_PASSWORD' }
+      subject(:password) { config.password }
+    end
+
+    it "behaves like a normal accessor" do
+      config.password = "my-new-value"
+      expect(config.password).to eq("my-new-value")
+    end
+  end
+
+  describe "#default_strategy" do
+    it_behaves_like "defaults to environment variable" do
+      let(:variable_name) { 'TESSA_DEFAULT_STRATEGY' }
+      subject(:default_strategy) { config.default_strategy }
+    end
+
+    it "behaves like a normal accessor" do
+      config.default_strategy = "my-new-value"
+      expect(config.default_strategy).to eq("my-new-value")
+    end
+  end
 end
