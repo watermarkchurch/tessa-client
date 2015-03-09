426db34c4f0c09ade68f1018973d109bd40137bd
Travis Petticrew <travis@petticrew.net>
2015-03-09 16:08:52 -0500

    Adds Asset::find method

--- lib/tessa/asset.rb
+++ lib/tessa/asset.rb
@@ -5,10 +5,23 @@ module Tessa
     attribute :id, Integer
     attribute :status, String
     attribute :strategy, String
-    attribute :meta, Hash
+    attribute :meta, Hash[Symbol => String]
     attribute :public_url, String
     attribute :private_url, String
     attribute :delete_url, String
 
+    def self.find(*ids,
+                  connection: Tessa.config.connection)
+      new_from_response connection.get("/assets/#{ids.join(",")}")
+    end
+
+    def self.new_from_response(response)
+      case json = JSON.parse(response.body)
+      when Array
+        json.map { |record| new record }
+      when Hash
+        new json
+      end
+    end
   end
 end
--- spec/tessa/asset_spec.rb
+++ spec/tessa/asset_spec.rb
@@ -48,4 +48,71 @@ RSpec.describe Tessa::Asset do
     end
   end
 
+  describe "#find" do
+    let(:faraday_stubs) {
+      Faraday::Adapter::Test::Stubs.new do |stub|
+        stub.get("/assets/#{id_query}") { |env| [200, {}, remote_response.to_json] }
+      end
+    }
+    let(:connection) { Faraday.new { |f| f.adapter :test, faraday_stubs } }
+
+    context "with a single id" do
+      let(:id_query) { "ID" }
+      let(:remote_response) { {} }
+
+      it "calls get('/assets/ID') on connection" do
+        expect(connection).to receive(:get).with("/assets/ID").and_call_original
+        described_class.find("ID", connection: connection)
+      end
+
+      it "returns an instance of Asset" do
+        response = described_class.find("ID", connection: connection)
+        expect(response).to be_a(described_class)
+      end
+
+      it "defaults connection to Tessa.config.connection" do
+        expect(Tessa.config).to receive(:connection).and_return(connection)
+        expect(connection).to receive(:get).and_call_original
+        described_class.find("ID")
+      end
+
+      context "with attributes in response" do
+        subject(:response) { described_class.find("ID", connection: connection) }
+        let(:remote_response) { args }
+
+        it "sets attributes on models" do
+          expect(response.id).to eq(args[:id])
+          expect(response.status).to eq(args[:status])
+          expect(response.meta).to eq(args[:meta])
+        end
+      end
+    end
+
+    context "with multiple ids" do
+      subject(:response) {
+        described_class.find("ID1", "ID2", connection: connection)
+      }
+      let(:remote_response) { [{}] }
+      let(:id_query) { "ID1,ID2" }
+
+      it "calls get('/assets/ID1,ID2') on connection" do
+        expect(connection).to receive(:get).with("/assets/ID1,ID2").and_call_original
+        response
+      end
+
+      it "returns instances of Asset" do
+        expect(response).to all(be_a(described_class))
+      end
+
+      context "with attributes in response" do
+        let(:remote_response) { [args, args] }
+
+        it "sets attributes on models" do
+          expect(response[0].id).to eq(args[:id])
+          expect(response[1].id).to eq(args[:id])
+        end
+      end
+    end
+  end
+
 end
