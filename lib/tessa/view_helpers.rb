7f140d23b2ef035ee52155101abb517b915ee7d3
Justin Powell <jpowellcs@gmail.com>
2015-05-12 13:08:41 -0500

    adds failure handler helper

--- lib/tessa/view_helpers.rb
+++ lib/tessa/view_helpers.rb
@@ -1,10 +1,18 @@
 module Tessa
   module ViewHelpers
     def tessa_image_tag(asset)
+      handle_failure(asset) do
+        image_tag(asset.private_url)
+      end
+    end
+
+    private
+
+    def handle_failure(asset)
       if asset.failure?
         content_tag(:div, asset.message, class: "tessa-asset-failure")
       else
-        image_tag(asset.private_url)
+        yield
       end
     end
   end
