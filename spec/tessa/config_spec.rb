0e8b79b3f11bb5161846afaa4317e3702d25abb5
Gordon <gburgett@watermark.org>
2022-06-27 13:09:35 -0500

    "config" is a reserved word in rspec_rails i guess

--- spec/tessa/config_spec.rb
+++ spec/tessa/config_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe Tessa::Config do
-  subject(:config) { Tessa::Config.new }
+  let(:cfg) { Tessa::Config.new }
 
   shared_examples_for "defaults to environment variable" do
     around { |ex| swap_environment_var(variable_name, 'from-env') { ex.run } }
@@ -19,63 +19,63 @@ RSpec.describe Tessa::Config do
   describe "#username" do
     it_behaves_like "defaults to environment variable" do
       let(:variable_name) { 'TESSA_USERNAME' }
-      subject(:username) { config.username }
+      subject { cfg.username }
     end
 
     it "behaves like a normal accessor" do
-      config.username = "my-new-value"
-      expect(config.username).to eq("my-new-value")
+      cfg.username = "my-new-value"
+      expect(cfg.username).to eq("my-new-value")
     end
   end
 
   describe "#password" do
     it_behaves_like "defaults to environment variable" do
       let(:variable_name) { 'TESSA_PASSWORD' }
-      subject(:password) { config.password }
+      subject { cfg.password }
     end
 
     it "behaves like a normal accessor" do
-      config.password = "my-new-value"
-      expect(config.password).to eq("my-new-value")
+      cfg.password = "my-new-value"
+      expect(cfg.password).to eq("my-new-value")
     end
   end
 
   describe "#url" do
     it_behaves_like "defaults to environment variable" do
       let(:variable_name) { 'TESSA_URL' }
-      subject(:url) { config.url }
+      subject { cfg.url }
     end
 
     it "behaves like a normal accessor" do
-      config.url = "my-new-value"
-      expect(config.url).to eq("my-new-value")
+      cfg.url = "my-new-value"
+      expect(cfg.url).to eq("my-new-value")
     end
   end
 
   describe "#strategy" do
     it_behaves_like "defaults to environment variable" do
       let(:variable_name) { 'TESSA_STRATEGY' }
-      subject(:strategy) { config.strategy }
+      subject { cfg.strategy }
     end
 
     it "uses the string 'default' when no envvar passed" do
-      expect(config.strategy).to eq("default")
+      expect(cfg.strategy).to eq("default")
     end
 
     it "behaves like a normal accessor" do
-      config.strategy = "my-new-value"
-      expect(config.strategy).to eq("my-new-value")
+      cfg.strategy = "my-new-value"
+      expect(cfg.strategy).to eq("my-new-value")
     end
   end
 
   describe "#connection" do
     it "is a Faraday::Connection" do
-      expect(config.connection).to be_a(Faraday::Connection)
+      expect(cfg.connection).to be_a(Faraday::Connection)
     end
 
-    context "with values configured" do
-      subject(:connection) { config.connection }
-      before { args.each { |k, v| config.send("#{k}=", v) } }
+    context "with values cfgured" do
+      subject { cfg.connection }
+      before { args.each { |k, v| cfg.send("#{k}=", v) } }
       let(:args) {
         {
           url: "http://tessa.test",
@@ -85,28 +85,28 @@ RSpec.describe Tessa::Config do
       }
 
       it "sets faraday's url prefix to our url" do
-        expect(connection.url_prefix.to_s).to match(config.url)
+        expect(subject.url_prefix.to_s).to match(cfg.url)
       end
 
       context "with faraday spy" do
         let(:spy) { instance_spy(Faraday::Connection) }
         before do
           expect(Faraday).to receive(:new).and_yield(spy)
-          connection
+          subject
         end
 
         it "sets up url_encoded request handler" do
           expect(spy).to have_received(:request).with(:url_encoded)
         end
 
-        it "configures the default adapter" do
+        it "cfgures the default adapter" do
           expect(spy).to have_received(:adapter).with(:net_http)
         end
       end
     end
 
     it "caches the result" do
-      expect(config.connection.object_id).to eq(config.connection.object_id)
+      expect(cfg.connection.object_id).to eq(cfg.connection.object_id)
     end
   end
 end
