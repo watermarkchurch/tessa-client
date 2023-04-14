8f1d10f74d5499ce35cc0209d95f5d5718cb85fd
Gordon <gburgett@watermark.org>
2023-04-14 16:41:07 -0500

    Prevent form submit if uploads not complete

--- app/javascript/tessa/index.ts
+++ app/javascript/tessa/index.ts
@@ -125,6 +125,8 @@ class WCCDropzone extends BaseDropzone {
   }
 }
 
+const uploadPendingWarning = "File uploads have not yet completed. If you submit the form now they will not be saved. Are you sure you want to continue?"
+
 /**
  * Called on page load to initialize the Dropzone
  */
@@ -176,6 +178,24 @@ function tessaInit() {
       $item.find(`input[name="${inputName}"]`).val('')
     })
   })
+
+  $('form:has(.tessa-upload)').each((i, form) => {
+    const $form = $(form)
+    $form.on('submit', (event: any) => {
+      let safeToSubmit = true
+      $form.find('.tessa-upload').each((j, dropzoneElement) => {
+        (dropzoneElement.dropzone.files as WCCDropzoneFile[]).forEach((file) => {
+          if (file.status && file.status != WCCDropzone.SUCCESS) {
+            safeToSubmit = false
+          }
+        })
+      })
+      if (!safeToSubmit && !confirm(uploadPendingWarning)) {
+        // prevent form submission
+        return false
+      }
+    })
+  })
 }
 
 /**
@@ -220,94 +240,3 @@ function accept(file: WCCDropzoneFile, done: (error?: string | Error) => void) {
 }
 
 $(tessaInit)
-
-// WCC.Dropzone.uploadPendingWarning =
-//   "File uploads have not yet completed. If you submit the form now they will
-//   not be saved. Are you sure you want to continue?"
-
-// WCC.Dropzone.prototype.defaultOptions.accept = (file, done) ->
-//   dz = $(file._removeLink).closest('.tessa-upload').first()
-//   tessaParams = dz.data('tessa-params') or {}
-
-//   postData =
-//     name: file.name
-//     size: file.size
-//     mime_type: file.type
-
-//   postData = $.extend postData, tessaParams
-
-//   FileChecksum.create file, (error, checksum) ->
-//     return done(error) if error
-
-//     postData['checksum'] = checksum
-
-//     $.ajax '/tessa/uploads',
-//       type: 'POST',
-//       data: postData,
-//       success: (response) ->
-//         file.uploadURL = response.upload_url
-//         file.uploadMethod = response.upload_method
-//         file.uploadHeaders = response.upload_headers
-//         file.assetID = response.asset_id
-//         done()
-//       error: (response) ->
-//         done("Failed to initiate the upload process!")
-
-// window.WCC.tessaInit = (sel) ->
-//   sel = sel || 'form:has(.tessa-upload)'
-//   $(sel).each (i, form) ->
-//     $form = $(form)
-//     $form.bind 'submit', (event) ->
-//       safeToSubmit = true
-//       $form.find('.tessa-upload').each (j, dropzoneElement) ->
-//         $(dropzoneElement.dropzone.files).each (k, file) ->
-//           if file.status? and (file.status != WCC.Dropzone.SUCCESS)
-//             safeToSubmit = false
-//       if not safeToSubmit and not confirm(WCC.Dropzone.uploadPendingWarning)
-//         return false
-
-//   $('.tessa-upload', sel).each (i, item) ->
-//     $item = $(item)
-//     args =
-//       maxFiles: 1
-//       addRemoveLinks: true
-
-//     $.extend args, $item.data("dropzone-options")
-//     args.maxFiles = null if $item.hasClass("multiple")
-//     inputPrefix = $item.data("asset-field-prefix")
-
-//     dropzone = new WCC.Dropzone item, args
-
-//     $item.find('input[type="hidden"]').each (j, input) ->
-//       $input = $(input)
-//       mockFile = $input.data("meta")
-//       mockFile.accepted = true
-//       dropzone.options.addedfile.call(dropzone, mockFile)
-//       dropzone.options.thumbnail.call(dropzone, mockFile, mockFile.url)
-//       dropzone.emit("complete", mockFile)
-//       dropzone.files.push mockFile
-
-//     updateAction = (file) ->
-//       return unless file.assetID?
-//       inputID = "#tessa_asset_action_#{file.assetID}"
-//       actionInput = $(inputID)
-//       unless actionInput.length
-//         actionInput = $('<input type="hidden">')
-//           .attr
-//             id: inputID
-//             name: "#{inputPrefix}[#{file.assetID}][action]"
-//           .appendTo item
-
-//       actionInput.val file.action
-
-//     dropzone.on 'success', (file) ->
-//       file.action = "add"
-//       updateAction(file)
-
-//     dropzone.on 'removedfile', (file) ->
-//       file.action = "remove"
-//       updateAction(file)
-
-
-// $ ->
-//   window.WCC.tessaInit()
