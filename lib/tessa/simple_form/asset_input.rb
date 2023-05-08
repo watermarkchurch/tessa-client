4e74706dea10712e557b1fe657c3097815d6eb51
Gordon <gburgett@watermark.org>
2023-05-08 14:12:49 -0500

    Fix when controller rejects change w/ errors

--- lib/tessa/simple_form/asset_input.rb
+++ lib/tessa/simple_form/asset_input.rb
@@ -17,27 +17,51 @@ module Tessa
 
     def hidden_fields_for(attribute_name)
       asset = object.public_send(attribute_name)
-      unless asset&.key.present?
-        return @builder.hidden_field("#{attribute_name}", value: nil)
+      if asset&.is_a?(String)
+        # This happens if the controller rejects the change with errors and re-renders the form.
+        # In this case our field value would be the signed upload ID.
+        return hidden_fileds_for_signed_id(attribute_name, asset)
       end
 
-      @builder.hidden_field("#{attribute_name}",
-        value: asset.key,
-        data: {
-          # These get read by the JS to populate the preview in Dropzone
-          meta: meta_for_asset(asset)
+      if asset&.key.present?
+        @builder.hidden_field("#{attribute_name}", {
+          value: asset.key,
+          data: {
+            meta: meta_for_blob(asset).merge({
+              # this allows us to find the hidden HTML input to remove it if we remove the asset
+              "signedID" => blob.key,
+            })
+          }
         })
+      end
+
+      return @builder.hidden_field("#{attribute_name}", value: nil)
     end
 
-    def meta_for_asset(asset)
+    def hidden_fileds_for_signed_id(attribute_name, signed_id)
+      if blob = ActiveStorage::Blob.find_signed(signed_id)
+        return @builder.hidden_field("#{attribute_name}", {
+          value: signed_id,
+          data: {
+            meta: meta_for_blob(blob).merge({
+              'signedID' => signed_id
+            })
+          }
+        })
+      end
+
+      # The form post sent some other string which was not a signed ID
+      return @builder.hidden_field("#{attribute_name}", value: nil)
+    end
+
+    # These get read by the JS to populate the preview in Dropzone
+    def meta_for_blob(blob)
       {
-        # this allows us to find the hidden HTML input to remove it if we remove the asset
-        "signedID" => asset.key,
-        "name" => asset.filename,
-        "size" => asset.byte_size,
-        "mimeType" => asset.content_type,
-        "url" => asset.service_url(disposition: :inline, expires_in: 1.hour),
-      }.to_json
+        "name" => blob.filename,
+        "size" => blob.byte_size,
+        "mimeType" => blob.content_type,
+        "url" => blob.service_url(disposition: :inline, expires_in: 1.hour),
+      }.as_json
     end
   end
 end
